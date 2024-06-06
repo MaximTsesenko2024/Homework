9d6605825954d2f86b24caed78bf54c2296f32e9
maxts <maksTS1991@yandex.ru>
2024-06-06 20:29:48 +0300

    Добавлена функция сортировки

--- Project_homework/module1hard.py
+++ Project_homework/module1hard.py
@@ -1,7 +1,7 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 print(students)
-students = list(students)
+students = sorted(list(students))
 print(students)
 my_dic = {k: v for k, v in zip(students, [sum(grades[x])/len(grades[x]) for x in range(len(grades))])}
 print('Средний бал по каждому ученику: ', my_dic)
