4ad9149ea5cf7465b52f3a04b9d89aefe0cef741
maxts <maksTS1991@yandex.ru>
2024-06-09 16:55:39 +0300

    Добавлено использование continue

--- Project_homework/module_2_3.py
+++ Project_homework/module_2_3.py
@@ -2,9 +2,10 @@ my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 print('Мой список: ', my_list)
 i = 0
 while not (my_list[i] < 0):
-    if my_list[i] > 0:
-        print('Положительное число из списка: ', my_list[i])
     i += 1
+    if my_list[i-1] == 0:
+        continue
+    print('Положительное число из списка: ', my_list[i-1])
     if i == len(my_list):
         break
 print('End')
