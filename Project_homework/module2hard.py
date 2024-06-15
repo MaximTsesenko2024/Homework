c75f016b36e18a015250741b36bbf7a486c1ca88
maxts <maksTS1991@yandex.ru>
2024-06-15 08:52:10 +0300

    Добавлен генератор первого числа для пароля

--- Project_homework/module2hard.py
+++ Project_homework/module2hard.py
@@ -1,17 +1,18 @@
 def find_dub(first_n):
-    res = []
+    res = ''
     for i in range(1, first_n):
         for j in range(i + 1, first_n):
             sum_numb = i + j
             if first_n % sum_numb == 0:
-                res.append(i)
-                res.append(j)
+                res += str(i)
+                res += str(j)
     return res
 
 
-first = int(input('Введите первое число от 3 до 20: '))
-# поиск пар чисел
-list_num = find_dub(first)
-print('Пароль: ', *list_num)
+#first = int(input('Введите первое число от 3 до 20: '))
+for first in range(3,21):
+    # поиск пар чисел
+    list_num = find_dub(first)
+    print(f'{first} - {list_num}')
 
 #
