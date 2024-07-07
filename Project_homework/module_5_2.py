0b0c4e491ac6581f0eb6d0b3ba54fee3012fbea5
maxts <maksTS1991@yandex.ru>
2024-07-07 14:05:36 +0300

    Добавлено решение задачи "Магические здания"

---
+++ Project_homework/module_5_2.py
@@ -0,0 +1,31 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor in range(1, self.number_of_floors+1):
+            for i in range(1, new_floor+1):
+                print(f'Этаж номер: {i}')
+        else:
+            print('Такого этажа не существует')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+h2.go_to(19)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
