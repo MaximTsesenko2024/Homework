deaba1f2d10b55499ff37f6fd09fafff41f2c30e
maxts <maksTS1991@yandex.ru>
2024-08-10 11:34:37 +0300

    Изменён способ объединения списков

--- Project_homework/module_9_2.py
+++ Project_homework/module_9_2.py
@@ -1,8 +1,9 @@
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
 first_result = [len(x) for x in first_strings if len(x) >= 5]
 print(first_result)
 second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
 print(second_result)
-third_result = {x: len(x) for x in first_strings + second_strings if not len(x) % 2}
+third_result = {y: len(y) for x in (first_strings, second_strings) for y in x if not len(y) % 2}
 print(third_result)
