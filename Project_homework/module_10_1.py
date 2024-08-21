e6e0dd960179525db69f96795f6f23fac0ece439
maxts <maksTS1991@yandex.ru>
2024-08-21 16:02:48 +0300

    Добавлено решение задачи "Потоковая запись в файлы"

---
+++ Project_homework/module_10_1.py
@@ -0,0 +1,42 @@
+from time import sleep
+from datetime import datetime
+from threading import Thread
+
+
+def write_words(word_count, file_name):
+    with open(file_name, mode="wt", encoding='utf-8') as file:
+        for i in range(word_count):
+            file.write(f"Какое-то слово № {i+1}\n")
+            sleep(0.1)
+    file.close()
+    print(f"Завершилась запись в файл {file_name}")
+
+
+print('Работа в функциях')
+time_start = datetime.now()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+time_end = datetime.now()
+time_rez = time_end - time_start
+print(time_rez)
+print('Работа в потоках')
+time_start = datetime.now()
+thr_first = Thread(target=write_words, args=(10, 'example5.txt'))
+thr_second = Thread(target=write_words, args=(30, 'example6.txt'))
+thr_third = Thread(target=write_words, args=(200, 'example7.txt'))
+thr_fourth = Thread(target=write_words, args=(100, 'example8.txt'))
+
+thr_first.start()
+thr_second.start()
+thr_third.start()
+thr_fourth.start()
+
+thr_first.join()
+thr_second.join()
+thr_third.join()
+thr_fourth.join()
+time_end = datetime.now()
+time_rez = time_end - time_start
+print(time_rez)
