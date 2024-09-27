615e3083f01ee987ea331a0c983ee8918417e827
maxts <maksTS1991@yandex.ru>
2024-09-27 10:29:00 +0300

    Добавлено решение задачи "Он мне ответил!"

---
+++ Project_homework/module_13_3.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = ""
+bot = Bot(token=api)
+dsp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dsp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dsp.message_handler()
+async def all_massages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+if __name__ == "__main__":
+    executor.start_polling(dsp, skip_updates=True)
