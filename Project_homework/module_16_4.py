6f5a42b54d802db3c7a3a36019717282d0478ade
maxts <maksTS1991@yandex.ru>
2024-10-30 12:11:51 +0300

    Добавлено решение задания "Модель пользователя"

---
+++ Project_homework/module_16_4.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, Path, HTTPException
+from typing import Annotated
+from pydantic import BaseModel
+
+api = FastAPI()
+
+users = []
+
+
+class User(BaseModel):
+    id: int = None  # номер пользователя
+    username: str = ''  # - имя пользователя
+    age: int = 0  # возраст пользователя
+
+
+@api.get('/')
+async def get_main_page() -> str:
+    return "Главная страница"
+
+
+@api.get('/users')
+async def get_all_users() -> list[User]:
+    return users
+
+
+@api.post('/user/{username}/{age}')
+async def create_user(
+        username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+        age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=45)]) -> User:
+    user_id = users[-1].id + 1 if len(users) > 0 else 1
+    new_user = User()
+    new_user.id = user_id
+    new_user.username = username
+    new_user.age = age
+    users.append(new_user)
+    return new_user
+
+
+@api.put('/user/{user_id}/{username}/{age}')
+async def update_user(
+        user_id: Annotated[int, Path(gt=0, le=120, description='Enter User ID', example=30)],
+        username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+        age: Annotated[int, Path(ge=18, le=120, description='Enter age', example=45)]) -> User:
+    index = None
+    for i in range(len(users)):
+        if users[i].id == user_id:
+            index = i
+            break
+    if index is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    users[index].username = username
+    users[index].age = age
+    return users[index]
+
+
+@api.delete('/user/{user_id}')
+async def delete_user(
+        user_id: Annotated[int, Path(gt=0, le=120, description='Enter User ID', example=30)]) -> User:
+    index = None
+    for i in range(len(users)):
+        if users[i].id == user_id:
+            index = i
+            break
+    if index is None:
+        raise HTTPException(status_code=404, detail="User was not found")
+    user = users.pop(index)
+    return user
