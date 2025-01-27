eb51e14e1bb1bf8e05b3ec409f2bee370a3af8cb
maxts <maksTS1991@yandex.ru>
2025-01-27 16:49:12 +0300

    Добавлено решение задания "Базовые структуры данных"

---
+++ Project_homework/base_structure_data.py
@@ -0,0 +1,20 @@
+# "1st program"
+print(9**0.5*5)
+
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# "3rd program"
+rez_1 = 2*2+2
+print(rez_1)
+rez_2 = 2*(2+2)
+print(rez_2)
+print(rez_1 == rez_2)
+
+# "4th program"
+st = '123.456'
+number = float(st)
+number = number * 10
+res = number % 10
+res = int(res)
+print(res)
